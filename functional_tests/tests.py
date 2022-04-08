a35b4d4258ec8cfab93bc211977a8abaa9e62021
mahdi <mahdi100th@gmail.com>
2022-04-08 10:03:49 +0430

    in FT, generate multiple urls for each user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
                 table = self.browser.find_element(By.ID, "id_list_table")
                 rows = table.find_elements(By.TAG_NAME, "tr")
                 self.assertIn(row_text, [row.text for row in rows])
-                return 
+                return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
@@ -43,4 +43,38 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail("Finish the test!")
 
+    def test_can_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/list/.+")
+        
+        self.browser.quit()
+        self.browser = webdriver.FireFox()
+        
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+        
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/list/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+        
