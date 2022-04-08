9810bf6fb4207e0f1cefc9123743e24ce312288c
mahdi <mahdi100th@gmail.com>
2022-04-08 10:47:51 +0430

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,11 @@ from lists.models import Item
 def home_page(request):
 
     if request.method == "POST":
-        new_item_text = request.POST["item_text"]
-        Item.objects.create(text=new_item_text)
-        return redirect("/")
-    items = Item.objects.all()
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/lists/the-only-list-in-the-world/")
+    return render(request, "home.html")
 
-    return render(request, "home.html", {"items": items})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, "list.html", {"items": items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name="home")
+    path("admin/", admin.site.urls),
+    path("", views.home_page, name="home"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]
