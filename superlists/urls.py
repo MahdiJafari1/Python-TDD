3015cb24e70fe0a03e69ea32d5b4fc51c34a5e5c
mahdi <mahdi100th@gmail.com>
2022-04-09 10:09:35 +0430

    separate url for lists app

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from lists import views
+from lists import urls as list_urls
+from lists import views as list_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:id>/", views.view_list, name="view_list"),
-    path("/lists/<int:id>/new_item", views.add_item, name="add_item"),
+    path("/lists/", include(list_urls)),
 ]
