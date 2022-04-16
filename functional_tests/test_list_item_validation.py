dab0cce1dc2e9a724752a01f1c1e1f6883a11ae8
mahdi <mahdi100th@gmail.com>
2022-04-16 09:33:46 +0430

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -35,3 +35,19 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy milk")
         self.wait_for_row_in_list_table("2: Make tea")
+
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy wellies")
+
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
+                "You've already got this in your list"
+            )
+        )
--- lists/models.py
+++ lists/models.py
@@ -10,3 +10,11 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default="")
     list = models.ForeignKey("List", default=None, on_delete=models.CASCADE)
+
+    class Meta: 
+        ordering = ("id",)
+        unique_together = ("list", "text",)
+        
+        
+    def __str__(self) -> str:
+        return self.text
